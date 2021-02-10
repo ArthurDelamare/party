43125bdd7822b047ef7983dac2e1fc9be29a2632
ArthurDelamare <arthur.delamare1@viacesi.fr>
2021-02-10 23:53:45 +0100

    feat: client server communication

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
