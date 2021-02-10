80aad508f55f2e4a6da536b9b002c2f5fa4cefe6
ArthurDelamare <arthur.delamare1@viacesi.fr>
2021-02-10 20:06:56 +0100

    feat: update to v3

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
-'use strict';
+let color = '#3aa757';
 
-console.log("background running!")
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
--- popup.js
+++ popup.js
@@ -1,10 +1,24 @@
-let changeColor = document.getElementById('changeColor');
+// Initialize butotn with users's prefered color
+let changeColor = document.getElementById("changeColor");
 
-changeColor.onclick = function(element) {
-  let color = 'black';
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
   });
-};
+});
+
+// The body of this function will be execuetd as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
