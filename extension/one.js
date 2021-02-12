1190179397ae0c4749fa7dd3b3cde21fb59b9fea
Arthur <arthur.delamare@canal-plus.com>
2021-02-12 16:22:00 +0100

    refactor: code cleaning

--- extension/one.js
+++ extension/one.js
@@ -1,3 +1,11 @@
 function sayHello(name) {
   console.log("Hello " + name + "!");
 }
+
+function createParty() {
+  console.log('Todo create party');
+}
+
+function joinParty() {
+  console.log('Todo join party');
+}
--- extension/popup.js
+++ extension/popup.js
@@ -1,25 +1,28 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+let createPartyButton = document.getElementById("createParty");
+let joinPartyButton = document.getElementById("joinParty");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+createPartyButton.addEventListener("click", async () =>  {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: setCreateParty,
   });
 });
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  sayHello('Arthur');
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+joinPartyButton.addEventListener("click", async () =>  {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setJoinParty,
   });
+});
+
+function setCreateParty() {
+  createParty();
+}
+
+function setJoinParty() {
+  joinParty();
 }
